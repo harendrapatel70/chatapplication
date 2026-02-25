cf5b250270eef700e2b4d452026d7606a710d2ba
harendrapatel70 <harendrapatel70@gmail.com>
2026-02-25 15:35:49 +0530

    all delete

--- mychatcore/mychatapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import(ChatHistory)
-from unfold.admin import ModelAdmin
-# Register your models here.
-
-@admin.register(ChatHistory)
-class ChatHistoryAdmin(ModelAdmin) :
-    list_display=("user","question","answer","created_at")
